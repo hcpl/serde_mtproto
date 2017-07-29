4fe3485ed76b5096be47cdce8460a60b60ad7b90
hcpl <hcpl.prog@gmail.com>
2017-07-29 16:25:12 +0300

    Export convenience functions to root

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,5 @@ pub mod ser;
 pub mod de;
 
 pub use error::{Error, Result};
-pub use ser::{Serializer};
-pub use de::{Deserializer};
+pub use ser::{Serializer, to_vec, to_writer};
+pub use de::{Deserializer, from_slice, from_reader};
