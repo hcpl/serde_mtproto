94d87082a43aa301b2b8a6bb2fe7e853a4d9b72e
hcpl <hcpl.prog@gmail.com>
2017-09-04 01:49:33 +0300

    Add more random-based benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -12,11 +12,67 @@ extern crate serde_mtproto_derive;
 extern crate test;
 
 
+use rand::{Rand, Rng};
 use serde_mtproto::{to_bytes, from_bytes};
 use test::Bencher;
 
 
 // PRIMITIVES
+
+#[bench]
+fn bool_serialize(b: &mut Bencher) {
+    let random_bool: bool = rand::random();
+
+    b.iter(|| {
+        to_bytes(&random_bool).unwrap();
+    });
+}
+
+#[bench]
+fn bool_deserialize(b: &mut Bencher) {
+    let random_bool: bool = rand::random();
+    let random_bool_serialized = to_bytes(&random_bool).unwrap();
+
+    b.iter(|| {
+        from_bytes::<bool>(&random_bool_serialized, None).unwrap();
+    });
+}
+
+#[bench]
+fn u8_serialize(b: &mut Bencher) {
+    let random_u8: u8 = rand::random();
+
+    b.iter(|| {
+        to_bytes(&random_u8).unwrap();
+    });
+}
+
+#[bench]
+fn u8_deserialize(b: &mut Bencher) {
+    let random_u8_serialized: [u8; 4] = [rand::random(), 0, 0, 0];
+
+    b.iter(|| {
+        from_bytes::<u8>(&random_u8_serialized, None).unwrap();
+    });
+}
+
+#[bench]
+fn u32_serialize(b: &mut Bencher) {
+    let random_u32: u32 = rand::random();
+
+    b.iter(|| {
+        to_bytes(&random_u32).unwrap();
+    });
+}
+
+#[bench]
+fn u32_deserialize(b: &mut Bencher) {
+    let random_u32_serialized: [u8; 4] = rand::random();
+
+    b.iter(|| {
+        from_bytes::<u32>(&random_u32_serialized, None).unwrap();
+    });
+}
 
 #[bench]
 fn i64_serialize(b: &mut Bencher) {
@@ -35,6 +91,61 @@ fn i64_deserialize(b: &mut Bencher) {
         from_bytes::<i64>(&random_i64_serialized, None).unwrap();
     });
 }
+
+#[bench]
+fn u64_serialize(b: &mut Bencher) {
+    let random_u64: u64 = rand::random();
+
+    b.iter(|| {
+        to_bytes(&random_u64).unwrap();
+    });
+}
+
+#[bench]
+fn u64_deserialize(b: &mut Bencher) {
+    let random_u64_serialized: [u8; 8] = rand::random();
+
+    b.iter(|| {
+        from_bytes::<u64>(&random_u64_serialized, None).unwrap();
+    });
+}
+
+#[bench]
+fn f32_serialize(b: &mut Bencher) {
+    let random_f32: f32 = rand::random();
+
+    b.iter(|| {
+        to_bytes(&random_f32).unwrap();
+    });
+}
+
+#[bench]
+fn f32_deserialize(b: &mut Bencher) {
+    let random_f32_serialized: [u8; 4] = rand::random();
+
+    b.iter(|| {
+        from_bytes::<f32>(&random_f32_serialized, None).unwrap();
+    });
+}
+
+#[bench]
+fn f64_serialize(b: &mut Bencher) {
+    let random_f64: f64 = rand::random();
+
+    b.iter(|| {
+        to_bytes(&random_f64).unwrap();
+    });
+}
+
+#[bench]
+fn f64_deserialize(b: &mut Bencher) {
+    let random_f64_serialized: [u8; 8] = rand::random();
+
+    b.iter(|| {
+        from_bytes::<f64>(&random_f64_serialized, None).unwrap();
+    });
+}
+
 
 // STRINGS
 
@@ -102,15 +213,15 @@ fn string_long_deserialize(b: &mut Bencher) {
     });
 }
 
-fn random_string<R: rand::Rng>(rng: &mut R, max_words_count: usize) -> String {
-    let lipsum_words_count: usize = rng.gen_range(0, max_words_count);
+fn random_string<R: Rng>(rng: &mut R, min_words_count: usize, max_words_count: usize) -> String {
+    let lipsum_words_count: usize = rng.gen_range(min_words_count, max_words_count);
 
     lipsum::lipsum(lipsum_words_count)
 }
 
 #[bench]
-fn random_string_serialize(b: &mut Bencher) {
-    let random_string = random_string(&mut rand::thread_rng(), 4096);
+fn random_string_short_serialize(b: &mut Bencher) {
+    let random_string = random_string(&mut rand::thread_rng(), 0, 32);
 
     b.iter(|| {
         to_bytes(&random_string).unwrap();
@@ -118,8 +229,8 @@ fn random_string_serialize(b: &mut Bencher) {
 }
 
 #[bench]
-fn random_string_deserialize(b: &mut Bencher) {
-    let random_string = random_string(&mut rand::thread_rng(), 4096);
+fn random_string_short_deserialize(b: &mut Bencher) {
+    let random_string = random_string(&mut rand::thread_rng(), 0, 32);
     let random_string_serialized = to_bytes(&random_string).unwrap();
 
     b.iter(|| {
@@ -127,7 +238,46 @@ fn random_string_deserialize(b: &mut Bencher) {
     });
 }
 
-// MISC
+#[bench]
+fn random_string_medium_serialize(b: &mut Bencher) {
+    let random_string = random_string(&mut rand::thread_rng(), 128, 512);
+
+    b.iter(|| {
+        to_bytes(&random_string).unwrap();
+    });
+}
+
+#[bench]
+fn random_string_medium_deserialize(b: &mut Bencher) {
+    let random_string = random_string(&mut rand::thread_rng(), 128, 512);
+    let random_string_serialized = to_bytes(&random_string).unwrap();
+
+    b.iter(|| {
+        from_bytes::<String>(&random_string_serialized, None).unwrap();
+    });
+}
+
+#[bench]
+fn random_string_long_serialize(b: &mut Bencher) {
+    let random_string = random_string(&mut rand::thread_rng(), 16384, 65536);
+
+    b.iter(|| {
+        to_bytes(&random_string).unwrap();
+    });
+}
+
+#[bench]
+fn random_string_long_deserialize(b: &mut Bencher) {
+    let random_string = random_string(&mut rand::thread_rng(), 16384, 65536);
+    let random_string_serialized = to_bytes(&random_string).unwrap();
+
+    b.iter(|| {
+        from_bytes::<String>(&random_string_serialized, None).unwrap();
+    });
+}
+
+
+// STRUCTS
 
 #[derive(Serialize, Deserialize, MtProtoIdentifiable)]
 #[id = "0xd594ba98"]
@@ -137,11 +287,12 @@ struct Foo {
     group: (i16, u64, i8),
 }
 
-impl rand::Rand for Foo {
-    fn rand<R: rand::Rng>(rng: &mut R) -> Foo {
+impl Rand for Foo {
+    fn rand<R: Rng>(rng: &mut R) -> Foo {
         Foo {
             bar: rng.gen(),
-            s: random_string(rng, 4096),
+            // Generate moderately long strings for reference
+            s: random_string(rng, 2048, 4096),
             group: rng.gen(),
         }
     }
