0eee429ce4f647841816a26bdd563cbbda32abd2
hcpl <hcpl.prog@gmail.com>
2018-03-14 16:23:47 +0200

    Improve benches of pointer-sized integers
    This way makes more sense.

--- benches/primitives.rs
+++ benches/primitives.rs
@@ -41,20 +41,33 @@ macro_rules! bench_primitive {
 }
 
 
+#[cfg(target_pointer_width = "16")]
+const ISIZE_SIZE: usize = 2;
+
+#[cfg(target_pointer_width = "32")]
+const ISIZE_SIZE: usize = 4;
+
+#[cfg(target_pointer_width = "64")]
+const ISIZE_SIZE: usize = 8;
+
+#[cfg(target_pointer_width = "128")]
+const ISIZE_SIZE: usize = 16;
+
+
 bench_primitive! {
     bool, bool_serialize => [u8; 4], bool_deserialize;
 
-    i8,    i8_serialize    => [u8; 4],  i8_deserialize;
-    i16,   i16_serialize   => [u8; 4],  i16_deserialize;
-    i32,   i32_serialize   => [u8; 4],  i32_deserialize;
-    i64,   i64_serialize   => [u8; 8],  i64_deserialize;
-    isize, isize_serialize => [u8; 16], isize_deserialize;    // 16 just in case of 128-bit machine
+    i8,    i8_serialize    => [u8; 4],          i8_deserialize;
+    i16,   i16_serialize   => [u8; 4],          i16_deserialize;
+    i32,   i32_serialize   => [u8; 4],          i32_deserialize;
+    i64,   i64_serialize   => [u8; 8],          i64_deserialize;
+    isize, isize_serialize => [u8; ISIZE_SIZE], isize_deserialize;
 
-    u8,    u8_serialize    => [u8; 4],  u8_deserialize;
-    u16,   u16_serialize   => [u8; 4],  u16_deserialize;
-    u32,   u32_serialize   => [u8; 4],  u32_deserialize;
-    u64,   u64_serialize   => [u8; 8],  u64_deserialize;
-    usize, usize_serialize => [u8; 16], usize_deserialize;    // same here
+    u8,    u8_serialize    => [u8; 4],          u8_deserialize;
+    u16,   u16_serialize   => [u8; 4],          u16_deserialize;
+    u32,   u32_serialize   => [u8; 4],          u32_deserialize;
+    u64,   u64_serialize   => [u8; 8],          u64_deserialize;
+    usize, usize_serialize => [u8; ISIZE_SIZE], usize_deserialize;
 
     f32, f32_serialize => [u8; 8], f32_deserialize;
     f64, f64_serialize => [u8; 8], f64_deserialize;
@@ -63,10 +76,10 @@ bench_primitive! {
     (u8, i16, f32, usize, f64, i64, u64, u32, i32, i8, isize, u16),    // <- truly random!
         all_numeric_primitives_tuple_serialize => [u8; 92], all_numeric_primitives_tuple_deserialize;
 
-    [u8; 32],    u8_array32_serialize    => [u8; 128], u8_array32_deserialize;
-    [i32; 32],   i32_array32_serialize   => [u8; 128], i32_array32_deserialize;
-    [usize; 32], usize_array32_serialize => [u8; 512], usize_array32_deserialize;
-    [f64; 32],   f64_array32_serialize   => [u8; 256], f64_array32_deserialize;
+    [u8; 32],    u8_array32_serialize    => [u8; 128],             u8_array32_deserialize;
+    [i32; 32],   i32_array32_serialize   => [u8; 128],             i32_array32_deserialize;
+    [usize; 32], usize_array32_serialize => [u8; ISIZE_SIZE * 32], usize_array32_deserialize;
+    [f64; 32],   f64_array32_serialize   => [u8; 256],             f64_array32_deserialize;
 }
 
 
