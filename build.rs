401be0e49d50c32b0339284e92865e4fd7a443be
hcpl <hcpl.prog@gmail.com>
2018-05-16 11:10:36 +0300

    Remove panicking in the build script
    If `rustc` is not found in the path then assume old `rustc` but still
    print a warning about this fact to stderr.

--- build.rs
+++ build.rs
@@ -1,7 +1,16 @@
 extern crate version_check;
 
+use std::io::{self, Write};
+
 fn main() {
-    if version_check::is_min_version("1.26.0").expect("couldn't query version info from rustc").0 {
-        println!("cargo:rustc-cfg=stable_i128");
+    match version_check::is_min_version("1.26.0") {
+        Some((true, _version)) => {
+            println!("cargo:rustc-cfg=stable_i128");
+        },
+        Some(_) => (),
+        None => {
+            // TODO: Use `eprintln!` after bumping minimal Rust version to 1.19
+            writeln!(io::stdout(), "couldn't query version info from rustc").unwrap();
+        },
     }
 }
